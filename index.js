4a738a53997bf7c17354314050184dbb70930284
joy <67973116+aquaj0y@users.noreply.github.com>
2024-05-29 00:29:29 +0100

    fix keypress looping issue

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 
-// listen for keydown, 
-window.addEventListener('keydown', (e) => {
-    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
-    const ovalKey = document.querySelector(`div[data-key='${e.keyCode}']`)
-
-    console.log('oval key is ', ovalKey)
-    ovalKey.classList.remove('elevation')
-    console.log(e.keyCode)
-    console.log('audio is ', audio)
-    audio.currentTime = 0; //set audio time to the start
-    audio.play()
-    audio.loop = false;
-    audio.onended = (e) => audio.pause()
-
-    
-    // if (audio.currentTime = audio.duration) {
-    //     ovalKey.classList.add('elevation')
-    //     console.log('time')
-    // }
-
-})
-
-window.addEventListener('keyup', (e) => {
-    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
-    const ovalKey = document.querySelector(`div[data-key='${e.keyCode}']`)
-    ovalKey.classList.add('elevation')
-    
-})
-
+document.addEventListener('DOMContentLoaded', (event) => {
+    let keysPressed = {};
+  
+    window.addEventListener('keydown', (e) => {
+      if (!keysPressed[e.keyCode]) {
+          keysPressed = true;
+          console.log('what is keysPressed here', keysPressed)
+        const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+        const ovalKey = document.querySelector(`div[data-key='${e.keyCode}']`);
+  
+        if (audio) {
+          console.log(e);
+          console.log('oval key is ', ovalKey);
+          ovalKey.classList.remove('elevation');
+          console.log(e.keyCode);
+          console.log('audio is ', audio);
+          audio.currentTime = 0; // set audio time to the start
+          audio.play();
+        }
+      }
+    });
+  
+    window.addEventListener('keyup', (e) => {
+      const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+      const ovalKey = document.querySelector(`div[data-key='${e.keyCode}']`);
+      
+      if (ovalKey) {
+        ovalKey.classList.add('elevation');
+      }
+        keysPressed[e.keyCode] = false;
+    });
+  });
+  
